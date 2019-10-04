665becee8ed2b1a88a77c8f59a981ca44ca7a214
전창석 <jcs191072@gmail.com>
2019-10-04 17:39:39 +0900

    수정

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = ["django_countries"]
 
 PROJECT_APPS = ["core.apps.CoreConfig", "users.apps.UsersConfig", "rooms.apps.RoomsConfig",
-        "reviews.apps.ReviewsConfig", "reservations.apps.ReservationsConfig"]
+        "reviews.apps.ReviewsConfig", "reservations.apps.ReservationsConfig",
+        "lists.apps.ListsConfig", "conversations.apps.ConversationsConfig"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db    import models
+from core         import models as core_model
+from users.models import User
+from rooms.models import Room
 
-# Create your models here.
+class List(core_model.TimeStampedModel):
+    name  = models.CharField(max_length        = 80)
+    user  = models.ForeignKey(User, on_delete  = models.CASCADE)
+    rooms = models.ManyToManyField(Room, blank = True)
+
+    def __str__(self):
+        return self.name
+
